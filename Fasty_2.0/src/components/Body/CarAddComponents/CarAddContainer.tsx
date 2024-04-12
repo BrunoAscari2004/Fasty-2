7dd0e8d9d7646feb01a52a260ea21552b31cf547
Bruno <brunoascar2004@gmail.com>
2024-04-12 17:43:02 -0300

    feat: adição do botao de adicionar carro e inicio da tela de input

--- Fasty_2.0/src/components/Body/CarAddComponents/CarAddContainer.tsx
+++ Fasty_2.0/src/components/Body/CarAddComponents/CarAddContainer.tsx
@@ -1,19 +1,81 @@
+import { useState } from "react";
 import { ListaCars } from "./ListaCars/ListaCars";
-import { BotaoAddCar, BotaoAddCarDiv, CarAddContainerStyled } from "./styles";
+import {
+  BotaoAddCar,
+  BotaoAddCarDiv,
+  BotaoSaveCar,
+  CarAddContainerStyled,
+  Input,
+  InputContainer,
+} from "./styles";
 import { VscDiffAdded } from "react-icons/vsc";
+import { IoMdClose } from "react-icons/io";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  TextField,
+} from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
 
 export const CarAddContainer = () => {
+  const [isCarDialogOpen, setIsCarDialogOpen] = useState(false);
+
+  const { control, handleSubmit } = useForm();
+
   return (
-    <CarAddContainerStyled>
-      <BotaoAddCarDiv>
-        <BotaoAddCar onClick={() => console.log("fui")}>
-          <VscDiffAdded size={24} />
-          <span>ADICIONAR CARRO</span>
-        </BotaoAddCar>
-      </BotaoAddCarDiv>
-      <div>
-        <ListaCars />
-      </div>
-    </CarAddContainerStyled>
+    <>
+      <CarAddContainerStyled>
+        <BotaoAddCarDiv>
+          <BotaoAddCar onClick={() => setIsCarDialogOpen(true)}>
+            <VscDiffAdded size={24} />
+            <span>ADICIONAR CARRO</span>
+          </BotaoAddCar>
+        </BotaoAddCarDiv>
+        <div>
+          <ListaCars />
+        </div>
+      </CarAddContainerStyled>
+      <Dialog
+        open={isCarDialogOpen}
+        onClose={() => {
+          setIsCarDialogOpen(false);
+        }}
+        maxWidth="md"
+      >
+        <DialogTitle
+          sx={{
+            fontSize: "24px",
+            color: "red",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          Adicionar carro
+          <IconButton>
+            <IoMdClose onClick={() => setIsCarDialogOpen(false)} />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <InputContainer style={{ width: 800 }}>
+            <Input>
+              <label htmlFor="carName">Nome:</label>
+              <Controller
+                name="carName"
+                control={control}
+                render={({ field }) => (
+                  <TextField size="small" {...field} color="primary" />
+                )}
+              />
+            </Input>
+          </InputContainer>
+        </DialogContent>
+        <DialogActions>
+          <BotaoSaveCar>SALVAR</BotaoSaveCar>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
--- Fasty_2.0/src/components/Body/CarAddComponents/styles.ts
+++ Fasty_2.0/src/components/Body/CarAddComponents/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const CarAddContainerStyled = styled.div`
   margin-top: 20px;
@@ -12,21 +12,14 @@ export const BotaoAddCarDiv = styled.div`
   margin-right: 20px;
 `;
 
-export const BotaoAddCar = styled.button`
-  height: 3.5rem;
-  width: 15rem;
+const baseButtonStyles = css`
+  font-size: 16px;
+
   color: red;
   background-color: white;
 
   border: solid red 2px;
   border-radius: 15px;
-
-  font-size: 16px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
 
   cursor: pointer;
 
@@ -46,4 +39,35 @@ export const BotaoAddCar = styled.button`
     background-size: 100%;
     transition: 0s;
   }
+`;
+
+export const BotaoAddCar = styled.button`
+  ${baseButtonStyles}
+  height: 3.5rem;
+  width: 15rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const BotaoSaveCar = styled.button`
+  ${baseButtonStyles}
+
+  padding: 8px;
+  margin-right: 20px;
+  margin-bottom: 10px;
+  transition: 0.1s;
+`;
+
+export const InputContainer = styled.div``;
+
+export const Input = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  font-size: 16px;
+  font-family: Roboto;
 `;
